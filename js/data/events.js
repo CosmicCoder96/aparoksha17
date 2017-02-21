f0147f09382d0852eaf063e5a50aed2a3c4d432c
Dewan <iit2015097@iiita.ac.in>
2017-02-21 17:47:25 +0530

    i am back

---
+++ js/data/events.js
@@ -0,0 +1,51 @@
+var events = [{
+    id: 'alkhwarizm',
+    name: 'Alkhwarizm',
+    organiser: 'Abhishek Deora',
+    coorganiser: 'Sahil Hindwani',
+    date: '23-25 March 17',
+    location: 'IIITA',
+    desc: 'Alkhwarizm is the annual coding event of IIIT’s Techfest Aparoksha. It is a prestigious \
+programming contest which brings you an opportunity to unleash your coding skills and solve \
+algorithmic problems. It is the place where optimization and proficiency matters, skills matter, hard \
+work matters, trying till the very end matters and winning loads of cash prizes matter !'
+    },
+    {
+        id: 'cfresh',
+        name: 'C-Fresh',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'IIITA',
+        desc: 'C-Fresh is the annual coding event specially designed to bring out the talent in the first year students. Exciting prizes await you !! '
+    },
+    {
+        id: 'itquiz',
+        name: 'IT Quiz',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'IIITA',
+        desc: 'The Mega IT quiz boasts of being biggest quizzing event of Aparoksha having  participation from numerous \
+candidates from the most prestigious institutions of the country. The participants engage in a battle \
+of wits amidst intense competition.'
+    },
+    {
+        id: 'hint',
+        name: 'Hack In The North',
+        organiser: 'Vishnu KS',
+        coorganiser: 'Saptak Sengupta',
+        date: '23-25 March 17',
+        location: 'CC3-IIITA',
+        desc: 'Hack In The North is the biggest student held hackathon in India. 300 students from all across India will come together in IIIT Allahabad this March and spend 36 hours building projects they are passionate about. Hack In The North is open for university and high school students.'
+    },
+    {
+        id: 'topbot',
+        name: 'Top-Bot',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'IIITA',
+        desc: 'placeholder text'
+    }
+];
