3efcdc86fb688f64554796baf99aafd30c91b9eb
shubham-padia <shubhamapadia@gmail.com>
2017-02-21 17:48:18 +0530

    झंडा जहाज आयोजन

---
+++ js/data/events.js
@@ -0,0 +1,49 @@
+var flagshipEvents = [{
+    id: 'alkhwarizm',
+    name: 'Alkhwarizm',
+    organiser: 'Abhishek Deora',
+    coorganiser: 'Sahil Hindwani',
+    date: '23-25 March 17',
+    location: 'IIITA',
+    desc: 'Alkhwarizm is the annual coding event of IIIT’s Techfest Aparoksha. It is a prestigious \
+programming contest which brings you an opportunity to unleash your coding skills and solve \
+algorithmic problems. It is the place where optimization and proficiency matters, skills matter, hard \
+work matters, trying till the very end matters and winning loads of cash prizes matter !'
+    },
+    {
+        id: 'cfresh',
+        name: 'C-Fresh',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'IIITA',
+        desc: 'C-Fresh is the annual coding event specially designed to bring out the talent in the first year students. Exciting prizes await you !! '
+    },
+    {
+        id: 'humblefoolcup',
+        name: 'Humblefoolcup',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'Topcoder',
+        desc: 'Topcoder in collaboration with Indian Institute of Information Technology Allahabad is excited to announce Humblefool Cup Algorithm Match in the fond memory of Harsha Suryanarayana (humblefool), one of the best Programmer of India.'
+    },
+    {
+        id: 'hint_event',
+        name: 'Hack In The North',
+        organiser: 'Vishnu KS',
+        coorganiser: 'Saptak Sengupta',
+        date: '23-25 March 17',
+        location: 'CC3-IIITA',
+        desc: 'Hack In The North is the biggest student held hackathon in India. 300 students from all across India will come together in IIIT Allahabad this March and spend 36 hours building projects they are passionate about. Hack In The North is open for university and high school students.'
+    },
+    {
+        id: 'topbot',
+        name: 'Top-Bot',
+        organiser: 'Abhishek Deora',
+        coorganiser: 'Sahil Hindwani',
+        date: '23-25 March 17',
+        location: 'IIITA',
+        desc: 'Aparoksha presents to you TopBot, a two round robot building contest. Call it a test of all the features of your bot, viz. mechanical, electronics and coding.This is the flagship event in electronics so you just can’t afford to miss this one out. This year, two interesting themes await you. Bolt, an obstacle evading contest will require your bot to overcome all the hindrances put in front of it in order to stand above the rest. The second theme Jarvis is a restricted contest only for first and second year students based on the line follower theme. So come in, show your engineering prowess and grab hold of the various prizes up for grabs.'
+    }
+];
